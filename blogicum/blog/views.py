e19cd6d5a4232f1a3a1e0194950a16a504979d27
Даниил Чумаков <f1v3nt5@gmail.com>
2023-06-24 13:22:52 +0300

    Fix pytest errors

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,15 +6,11 @@ from datetime import datetime
 def index(request):
     template = 'blog/index.html'
     dt_now = datetime.now()
-    post_list = Post.objects.values(
-        'id', 'category__title', 'category__slug',
-        'text', 'author__username', 'location',
-        'location__name', 'location__is_published',
-        'title', 'pub_date'
-    ).filter(
-        is_published=True, category__is_published=True,
+    post_list = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
         pub_date__lte=dt_now
-    ).order_by('-pub_date')[:5]
+    ).select_related().all().order_by('-pub_date')[:5]
     context = {
         'post_list': post_list
     }
@@ -24,15 +20,11 @@ def index(request):
 def post_detail(request, id):
     template = 'blog/detail.html'
     dt_now = datetime.now()
-    post = get_object_or_404(Post.objects.values(
-        'title', 'location', 'location__is_published',
-        'location__name', 'author__username',
-        'category__title', 'category__slug', 'text'
-    ).filter(
+    post = get_object_or_404(Post.objects.filter(
         pub_date__lte=dt_now,
-        is_published=True, category__is_published=True
-    ), pk=id)
-
+        is_published=True,
+        category__is_published=True
+    ).select_related().all(), pk=id)
     context = {
         'post': post
     }
@@ -42,15 +34,14 @@ def post_detail(request, id):
 def category_posts(request, category_slug):
     template = 'blog/category.html'
     dt_now = datetime.now()
-    post_list = get_list_or_404(Post.objects.values(
-        'category__title', 'category__slug', 'category__description',
-        'title', 'pub_date', 'location', 'location__is_published',
-        'location__name', 'author__username', 'text', 'id'
-    ).filter(
-        is_published=True, category__slug=category_slug,
+    post_list = get_list_or_404(Post.objects.filter(
+        is_published=True,
+        category__slug=category_slug,
         category__is_published=True,
         pub_date__lte=dt_now
-    ).order_by('-pub_date'))
+    ).select_related().all().order_by(
+        '-pub_date'
+    ))
     context = {
         'category': category_slug,
         'post_list': post_list

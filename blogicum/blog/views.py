58f79bf7fa366379783c9b7c2c0303d87f9ad0f4
Даниил Чумаков <f1v3nt5@gmail.com>
2023-06-26 01:49:51 +0300

    Do some minor adjustments

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,49 +1,54 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-from blog.models import Post
 from datetime import datetime
+
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from blog.models import Post, Category
+
+
+POSTS_COUNT = 5
+
+
+def post_pub_filter():
+    return Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+    )
 
 
 def index(request):
-    template = 'blog/index.html'
-    dt_now = datetime.now()
-    post_list = Post.objects.filter(
-        is_published=True,
-        category__is_published=True,
-        pub_date__lte=dt_now
-    ).select_related().all().order_by('-pub_date')[:5]
+    post_list = post_pub_filter().select_related(
+        'location', 'category', 'author'
+    ).order_by('-pub_date')[:POSTS_COUNT]
     context = {
         'post_list': post_list
     }
-    return render(request, template, context)
+    return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id):
-    template = 'blog/detail.html'
-    dt_now = datetime.now()
-    post = get_object_or_404(Post.objects.filter(
-        pub_date__lte=dt_now,
-        is_published=True,
-        category__is_published=True
-    ).select_related().all(), pk=id)
+    post = get_object_or_404(post_pub_filter().select_related(
+        'location', 'category', 'author'
+    ), pk=id)
     context = {
         'post': post
     }
-    return render(request, template, context)
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    dt_now = datetime.now()
-    post_list = get_list_or_404(Post.objects.filter(
+    category = get_object_or_404(Category.objects.values(
+        'title', 'description', 'slug'
+    ).filter(is_published=True,  slug=category_slug))
+    post_list = Post.objects.filter(
         is_published=True,
         category__slug=category_slug,
-        category__is_published=True,
-        pub_date__lte=dt_now
-    ).select_related().all().order_by(
+        pub_date__lte=datetime.now()
+    ).select_related('category').order_by(
         '-pub_date'
-    ))
+    )
     context = {
-        'category': category_slug,
+        'category': category,
         'post_list': post_list
     }
-    return render(request, template, context)
+    return render(request, 'blog/category.html', context)
